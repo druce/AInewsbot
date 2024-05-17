1fe5f7e640aef10b0d8ee1bd758f8d96ebde05e1
Druce Vertes <drucev@gmail.com>
2024-05-17 16:54:03 -0400

    move utilities to BB_agent_tool

--- BB_agent_tool.py
+++ BB_agent_tool.py
@@ -8,6 +8,10 @@
 # but it adds a layer of complexity, is a very thin abstraction, and hides what's going on
 
 import json
+from ainb_const import MODEL, bb_agent_system_prompt
+from IPython.display import Markdown, display
+
+import openai
 
 # import openbb
 from openbb import obb
@@ -147,6 +151,202 @@ class BB_agent_tool(object):
 
             examples.append(f"{self.name}({pstr}) -> {return_value}")
             return "; ".join(examples)
+
+
+# utility functions to call chatgpt with tools
+def eval_tool(tool_call, verbose=True):
+    """
+    Given an OpenAI tool_call response,
+    evaluates the tool function using the arguments provided by OpenAI,
+    and returns the message to send back to OpenAI, including the function return value.
+
+    Args:
+        tool_call (object): The OpenAI tool_call response.
+
+    Returns:
+        dict: The message to send back to OpenAI, containing the tool_call_id, role, name, and value returned by the tool call.
+
+    """
+    try:
+        function_name = tool_call.function.name
+        # look up the function based in global tools on the name
+        fn = BB_agent_tool.agent_registry[function_name]
+        # make the tool call's json args into a dict
+        kwargs = json.loads(tool_call.function.arguments)
+
+        if verbose:
+            print(f"{function_name}({str(kwargs)}) -> ", end="")
+        # call function with the args and return value
+        fn_value = fn(**kwargs)
+        if type(fn_value) is list or type(fn_value) is dict:
+            fn_value = str(fn_value)
+        if verbose:
+            output = str(fn_value)
+            if len(output) > 100:
+                output = output[:100] + "..."
+            print(output)
+
+        return {
+            "tool_call_id": tool_call.id,
+            "role": "tool",
+            "name": tool_call.function.name,
+            "content": fn_value,
+        }
+    except Exception as exc:
+        return f"Error: {exc}"
+
+
+def get_response(client, messages, tools, model=MODEL, json_format=False):
+    """
+    Get a single response from ChatGPT based on a chain of messages.
+
+    Args:
+        client: The OpenAI client.
+        messages (list): A list of message objects representing the conversation history.
+        json_format(boolean): True if JSON response requested. (Last message must express the request for JSON response.)
+
+    Returns:
+        dict: A response object containing the generated response from ChatGPT.
+
+    Raises:
+        OpenAIError: If there is an error during the API call.
+
+    Example:
+        >>> messages = [
+        ...     {"role": "system", "content": "You are a helpful assistant."},
+        ...     {"role": "user", "content": "What's the weather like today?"},
+        ... ]
+        >>> response = get_response(messages)
+    """
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        # can't pass None, need to pass NotGiven
+        tools=tools if tools else openai.NotGiven(),
+        # tool_choice="auto",  # auto is default, but we'll be explicit
+        response_format={"type": "json_object"} if json_format else None,
+    )
+
+    return response
+
+
+def get_response_and_eval(client, messages, tools=[], json_format=False, raw=False, verbose=False):
+    """
+    Sends a list of messages to OpenAI and returns the response.
+    If tool calls are returned, calls all the tools and sends the values back to OpenAI.
+    If further tool calls returned, iterates until no more tool calls are returned and
+    'stop' is returned as finish_reason, then returns the response.
+
+    Args:
+        client: The OpenAI client.
+        messages (list): A list of messages to send to OpenAI.
+        json_format (boolean): If the final response should be in JSON format.
+        raw (boolean): after last tool is called return raw data response that enabled answering the question
+        verbose (bool, optional): If True, prints additional information. Defaults to False.
+
+    Returns:
+        response: The final response object returned by OpenAI.
+
+    Raises:
+        None
+
+    """
+    response = get_response(
+        client, messages, tools=tools, json_format=json_format)
+    choice = response.choices[0]
+    response_message = choice.message
+    finish_reason = choice.finish_reason
+
+    if verbose:
+        print(choice)
+
+    while finish_reason != 'stop':
+        # Extend conversation with assistant's reply
+        messages.append(response_message)
+        if finish_reason == 'tool_calls':
+            tool_calls = response_message.tool_calls
+            if verbose:
+                print(tool_calls)
+            # Call the tools and add all return values as messages
+            for tool in tool_calls:
+                messages.append(eval_tool(tool, verbose=True))
+            # Get next response
+            response = get_response(
+                client, messages, tools=tools, json_format=json_format)
+            choice = response.choices[0]
+            response_message = choice.message
+            finish_reason = choice.finish_reason
+            if verbose:
+                output = str(choice)
+                output = output[:1000] + \
+                    "..." if len(output) > 1000 else output
+                print(output)
+        else:
+            print('finish_reason: ', finish_reason)
+            break
+
+    if raw:
+        # probably want to process that message and return call signature + value
+        return messages[-1]
+    else:
+        return response
+
+
+def agent_query(client, user_message, raw=False, verbose=True):
+    """
+    Send a user message to OpenAI and retrieve the response, calling all tools until done.
+
+    Args:
+        user_message (str): The message from the user.
+        raw (boolean): after last tool is called return raw data response that enabled answering the question
+        verbose (bool, optional): Display intermediate tool calls and return values. Defaults to False.
+
+    Returns:
+        str: The response from the agent.
+
+
+    Example:
+        >>> agent_query("Hello")
+        'Hello! How can I assist you today?'
+    """
+
+    # recompute system prompt, adding tool metadata i.e. descriptions of available tools to system prompt
+    tool_descs = ""
+    openai_tools = []
+    for v in BB_agent_tool.agent_registry.values():
+        t = v.tooldict
+        openai_tools.append(t)
+        tname = t['function']['name']
+        tdesc = t['function']['description']
+        tool_descs += f"{tname} : {tdesc}"
+        if v.example_code:
+            tool_descs += f" Usage: {v.example_code}"
+        tool_descs += "\n---\n"
+    # tool_descs = "\n".join([f"{tool['function']['name']} : {tool['function']['description']}" for tool in tools.values()])
+    current_system_prompt = bb_agent_system_prompt + f"""
+
+Available tools, with name, description, and calling example, delimited by ---:
+{tool_descs}
+    """
+    # print(bb_agent_system_prompt)
+    RETRIES = 3
+    for retry in range(RETRIES):
+        try:
+            if retry:
+                print(f"retrying, attempt {retry + 1}")
+            messages = [{"role": "system", "content": current_system_prompt},
+                        {"role": "user", "content": user_message}]
+            response = get_response_and_eval(
+                client, messages, tools=openai_tools, raw=raw, verbose=verbose)
+            response_str = response.choices[0].message.content
+            # escape stuff that is interpreted as latex
+            response_str = response_str.replace("$", "\\\$")
+            display(Markdown(response_str))
+            # success, exit retry loop
+            break
+        except Exception as exc:
+            print(exc)
 
 
 if __name__ == "__main__":
