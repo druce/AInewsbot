d7c1820b91489576fd228149af28b244af187a0e
Druce Vertes <drucev@gmail.com>
2025-02-17 22:50:11 -0500

    remove unused chrome plugin

--- AIbot_chrome/popup.js
+++
@@ -1,9 +0,0 @@
-// popup.js
-
-document.getElementById('start-button').addEventListener('click', () => {
-    // Send a message to the background script to start the process
-    chrome.runtime.sendMessage({ action: 'startFetchingNews' });
-
-    // Optionally, close the popup after the button is clicked
-    window.close();
-});
